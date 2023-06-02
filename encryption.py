ec1aca2493439ced51bdecaee7a267ac76f31b4d
Ernie de Magtige <91462826+ErniedM@users.noreply.github.com>
2023-06-02 16:12:58 +0200

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -19,16 +19,3 @@ class Encryption:
         decrypted_data = self.fernet.decrypt(encrypted_data)
         with open(output_path, "wb") as file:
             file.write(decrypted_data)
-    
-    # def __init__(self, key_path):
-    #     with open(key_path, "rb") as file:
-    #         key = file.read()
-    #     self.fernet = Fernet(key)
-
-    # def encrypt(self, data):
-    #     encrypted_data = self.fernet.encrypt(data.encode())
-    #     return encrypted_data
-
-    # def decrypt(self, encrypted_data):
-    #     decrypted_data = self.fernet.decrypt(encrypted_data)
-    #     return decrypted_data.decode()
